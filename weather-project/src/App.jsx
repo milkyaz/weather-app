7943f08481f392b4c1f9bb677e2c603dcf0f3717
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-10-17 20:27:29 +0400

    first commit

---
+++ weather-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  //определние местоположения
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }, [lat, long]);
+
+  return <></>;
+}
+
+export default App;
